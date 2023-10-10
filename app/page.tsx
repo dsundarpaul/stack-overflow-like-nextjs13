975721b1e9e7e4d8b6d26e3359035ed47f0a7969
dsundarpaul <109967226+dsundarpaul@users.noreply.github.com>
2023-10-10 23:23:21 +0530

    routing and atuh

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Nest.js 12 we&apos;re comming</h1>
-      <h1 className="h2-bold">Nest.js 12 we&apos;re comming</h1>
-      <h1 className="h3-bold">Nest.js 12 we&apos;re comming</h1>
-    </div>
-  );
-}
