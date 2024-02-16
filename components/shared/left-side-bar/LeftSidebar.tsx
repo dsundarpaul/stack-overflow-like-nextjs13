d458b85da15c0066d0ee69a509a64bc00b06bea1
dsundarpaul <109967226+dsundarpaul@users.noreply.github.com>
2024-02-16 12:12:15 +0530

    ask_questions

--- components/shared/left-side-bar/LeftSidebar.tsx
+++ components/shared/left-side-bar/LeftSidebar.tsx
@@ -18,31 +18,30 @@ const LeftSidebar = () => {
             (pathname.includes(item.route) && item.route.length > 1) ||
             pathname === item.route;
           return (
-            <>
-              <Link
-                href={item.route}
+            <Link
+              key={item.label}
+              href={item.route}
+              className={`${
+                isActive
+                  ? "primary-gradient rounded-lg text-light-900"
+                  : "text-dark300_light900"
+              } flex items-center justify-start gap-4 bg-transparent p-4`}
+            >
+              <Image
+                src={item.imgURL}
+                alt={item.label}
+                width={20}
+                height={20}
+                className={`${isActive ? "" : "invert-colors"}`}
+              />
+              <p
                 className={`${
-                  isActive
-                    ? "primary-gradient rounded-lg text-light-900"
-                    : "text-dark300_light900"
-                } flex items-center justify-start gap-4 bg-transparent p-4`}
+                  isActive ? "base-bold" : "base-medium"
+                } max-lg:hidden`}
               >
-                <Image
-                  src={item.imgURL}
-                  alt={item.label}
-                  width={20}
-                  height={20}
-                  className={`${isActive ? "" : "invert-colors"}`}
-                />
-                <p
-                  className={`${
-                    isActive ? "base-bold" : "base-medium"
-                  } max-lg:hidden`}
-                >
-                  {item.label}
-                </p>
-              </Link>
-            </>
+                {item.label}
+              </p>
+            </Link>
           );
         })}
       </div>
